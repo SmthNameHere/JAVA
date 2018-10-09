c1037d7bdf0d9190e5aca3f8f7e6a60c1a59ac18
katekorabanovskaya <43904292+katekorabanovskaya@users.noreply.github.com>
2018-10-09 18:02:33 +0300

    add main phrase in First.java

--- Labs_Vik/Lab1/First.java
+++ Labs_Vik/Lab1/First.java
@@ -3,5 +3,6 @@ package lab1;
 public class First {
     public static void main (String[] args) {
         System.out.println("This is Java, Hello");
+        System.out.println("Hello, world :)");
     }
 }
